eaf2bc5b5bf832c31246bcc09e18f5ed776659a5
OmarWaled21 <omar.waled484@gmail.com>
2025-02-10 18:40:00 +0200

    add background image

--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -16,6 +16,8 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
   @override
   void dispose() {
     super.dispose();
@@ -28,38 +30,56 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(forceMaterialTransparency: true),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: context.screenWidth * 0.04),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: context.screenHeight * 0.02,
-          children: [
-            Text(
-              'Sign Up.',
-              style: TextStyle(
-                fontSize: context.devicePixelRatio * 13,
-                fontWeight: FontWeight.bold,
+      body: Stack(
+        clipBehavior: Clip.antiAlias,
+        children: [
+          Positioned(
+            bottom: -120,
+            left: -60,
+            child: Image.asset(
+              'assets/image.png',
+              width: context.screenWidth,
+              opacity: const AlwaysStoppedAnimation(.2),
+            ),
+          ),
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: context.screenWidth * 0.04),
+            child: Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: context.screenHeight * 0.02,
+                children: [
+                  Text(
+                    'Sign Up.',
+                    style: TextStyle(
+                      fontSize: context.devicePixelRatio * 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: context.screenHeight * 0.001),
+                  AuthTextField(
+                    hintText: 'Name',
+                    controller: _nameController,
+                  ),
+                  AuthTextField(
+                    hintText: 'Email',
+                    controller: _emailController,
+                  ),
+                  AuthTextField(
+                    hintText: 'Password',
+                    controller: _passwordController,
+                    isSecured: true,
+                  ),
+                  SizedBox(height: context.screenHeight * 0.01),
+                  AuthButton(onTap: () {}, title: 'Sign up'),
+                  const AuthCheckSigning(isSigningUp: true)
+                ],
               ),
             ),
-            SizedBox(height: context.screenHeight * 0.001),
-            AuthTextField(
-              hintText: 'Name',
-              controller: _nameController,
-            ),
-            AuthTextField(
-              hintText: 'Email',
-              controller: _emailController,
-            ),
-            AuthTextField(
-              hintText: 'Password',
-              controller: _passwordController,
-              isSecured: true,
-            ),
-            SizedBox(height: context.screenHeight * 0.01),
-            AuthButton(onTap: () {}, title: 'Sign up'),
-            const AuthCheckSigning(isSigningUp: true)
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
